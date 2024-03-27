7d54ac33e56a70b774977c6ce98d3067b40adea5
shaik.dariyabi <157482022+shaikdariyabi@users.noreply.github.com>
2024-03-27 17:08:54 +0000

    items list created

---
+++ items/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return  <div>
+    <h1>Items List</h1>
+<section className="User">
+    <Item name="Dal" price="50" quality="Good"/>
+    <Item  name="Rice" price="150" quality="bad"  />
+    </section>
+  </div>
+}
+
+
+
+function Item(props) {
+  return <div className="item">
+    <h1>{props.name}</h1>
+    <p> Price: {props.price}</p>
+    <p> Quality: {props.quality}</p>
+
+
+
+  </div>
+
+}
+
+export default App;
